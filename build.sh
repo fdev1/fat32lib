4fd3c7482489e550c0f02de37f012afead23ee18
Fernando Rodriguez <frodriguez.developer@outlook.com>
2014-09-26 15:51:07 -0400

    Some fixes to build scripts.

--- build.sh
+++ build.sh
@@ -1,16 +1,64 @@
 #!/bin/sh
 
 cd dspic_hal
-make RM="rm -f" CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33f
-make RM="rm -f" CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33e
+echo
+echo 'Building dspic_hal for P33F...'
+make \
+	RM="rm -f" \
+	CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc \
+	AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33f
+
+echo
+echo 'Building dspic_hal for P33E...'
+make \
+	RM="rm -f" \
+	CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc \
+	AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33e
+
 cd ../fat32lib
-make RM="rm -f" CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33f
-make RM="rm -f" CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33e
+echo
+echo 'Building fat32lib for P33F...'
+make \
+	RM="rm -f" \
+	CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc \
+	AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33f
+
+echo
+echo 'Building fat32lib for P33E...'
+make \
+	RM="rm -f" \
+	CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc \
+	AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33e
+
 cd ../sdlib
-make RM="rm -f" CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33f
-make RM="rm -f" CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33e
+echo
+echo 'Building sdlib for dsPIC33F...'
+make \
+	RM="rm -f" \
+	CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc \
+	AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33f
+
+echo
+echo 'Building sdlib for dsPIC33E...'
+make \
+	RM="rm -f" \
+	CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc \
+	AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33e
+
 cd ../smlib
-make RM="rm -f" CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33f
-make RM="rm -f" CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33e
+echo
+echo 'Building smlib for dsPIC33F...'
+make \
+	RM="rm -f" \
+	CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc \
+	AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33f
+
+echo
+echo 'Building smlib for dsPIC33E...'
+make \
+	RM="rm -f" \
+	CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc \
+	AR=/opt/microchip/xc16/v1.21/bin/xc16-ar p33e
+
 echo
 
